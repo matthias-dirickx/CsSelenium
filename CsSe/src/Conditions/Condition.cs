99ff656519ea555a291c12024f9d908ec2701116
Dirickx <Matthias.Dirickx@ordina.be>
2019-05-29 14:25:17 +0200

    added license header for GNU v3 public license (do whatever you want except close-sourcing it - Let it be free!)

--- CsSe/src/Conditions/Condition.cs
+++ CsSe/src/Conditions/Condition.cs
@@ -1,7 +1,24 @@
-﻿using OpenQA.Selenium;
+﻿/*
+ * Copyright 2019 Matthias Dirickx
+ * 
+ * This file is part of CsSeSelenium.
+ * 
+ * CsSeSelenium is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * CsSeSelenium is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * WITHOUT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with CsSeSelenium.
+ * 
+ * If not, see http://www.gnu.org/licenses/.
+ */
 
-using CsSeleniumFrame.src.Core;
-using System.Collections.Generic;
+using OpenQA.Selenium;
 
 namespace CsSeleniumFrame.src.Conditions
 {
--- CsSe/src/Conditions/Operators/NotCondition.cs
+++ CsSe/src/Conditions/Operators/NotCondition.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * Copyright 2019 Matthias Dirickx
+ * 
+ * This file is part of CsSeSelenium.
+ * 
+ * CsSeSelenium is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * CsSeSelenium is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * WITHOUT even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with CsSeSelenium.
+ * 
+ * If not, see http://www.gnu.org/licenses/.
+ */
+
 using OpenQA.Selenium;
 
 namespace CsSeleniumFrame.src.Conditions
